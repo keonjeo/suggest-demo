5091c308bab747e512fa1717b5b4911d5a92e4fb
Keon Ye <jnuyeyizhou@gmail.com>
2019-10-01 11:08:15 +0800

    refactor RecommendService

--- app/services/recommend_service.rb
+++ app/services/recommend_service.rb
@@ -11,13 +11,12 @@ class RecommendService
 
   def run
     post_ids = PostNotifier.where(notifier_id: @user_id).pluck(:post_id)
-    caculate(post_ids, SUGGEST_STRONG_WEIGHT, @user_id)
+    caculate(post_ids, SUGGEST_STRONG_WEIGHT, [@user_id])
 
-    post_ids.each do |post_id|
-      user = Post.find(post_id).user
-      other_post_ids = user.posts.pluck(:id) - post_ids
-      caculate(other_post_ids, SUGGEST_WEAK_WEIGHT, user.id)
-    end
+    user_ids = Post.where(id: post_ids).pluck(:user_id)
+    other_post_ids = Post.where(user_id: user_ids).pluck(:id) - post_ids
+
+    caculate(other_post_ids, SUGGEST_WEAK_WEIGHT, user_ids)
 
     @result = @result.sort_by { |_, v| v }.reverse.to_h
     value = @result.map {|k,v|k}
@@ -28,11 +27,10 @@ class RecommendService
   private
 
   def caculate(post_ids, weight, itself)
-    post_ids.each do |post_id|
-      user_ids = PostNotifier.where(post_id: post_id).pluck(:notifier_id) - [itself]
-      user_ids.each do |user_id|
-        @result[user_id] = @result[user_id].to_i + weight
-      end
+    user_ids = PostNotifier.where(post_id: post_ids).pluck(:notifier_id)
+    user_ids.each do |user_id|
+      next if itself.include?(user_id)
+      @result[user_id] = @result[user_id].to_i + weight
     end
   end
 end
